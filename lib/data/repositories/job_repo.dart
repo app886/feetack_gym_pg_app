c8a5a3a3ca847e3587b31f0941c3ca3acd5aeec1
lifeinfotech123 <lifeinfotechmobilework@gmail.com>
2026-09-21 18:27:33 +0530

    Updated project files with referral sharing and redesigns

--- lib/data/repositories/job_repo.dart
+++ lib/data/repositories/job_repo.dart
@@ -17,4 +17,20 @@ class JobRepo {
   Future<Response> shareReferral(Map<String, dynamic> data) async {
     return await apiClient.postData("shared-referrals", "shareReferral", data);
   }
+
+  Future<Response> getJobDetailsByReferral(String referralCode) async {
+    Response response = await apiClient.getData("hrms/shared-referrals/$referralCode", "getJobDetailsByReferral");
+    if (response.statusCode != 200 && response.statusCode != 201) {
+      response = await apiClient.getData("shared-referrals/$referralCode", "getJobDetailsByReferral");
+    }
+    return response;
+  }
+
+  Future<Response> applyJob(Map<String, dynamic> data) async {
+    Response response = await apiClient.postData("hrms/job-applications", "applyJob", data);
+    if (response.statusCode != 200 && response.statusCode != 201) {
+      response = await apiClient.postData("job-applications", "applyJob", data);
+    }
+    return response;
+  }
 }
